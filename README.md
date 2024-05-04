a717b4d85e4bc35a0da3142e669a8f97034e0d4d
Fredrik Bakken <Fredrik.Bakken@gmail.com>
2024-05-04 15:39:41 +0200

    Initialize project example for lakefs with datafusion
    Signed-off-by: Fredrik Bakken <Fredrik.Bakken@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# lakefs_datafusion
+# LakeFS with Apache DataFusion
+
+![](asset/logo.png)
+
+## Prerequisties
+
+This guide assumes that you have already installed the following:
+
+- Docker
+- Docker Compose
+- Rust ([v1.78.0](https://blog.rust-lang.org/2024/05/02/Rust-1.78.0.html) used for development)
+
+## Docker
+
+The LakeFS service is accessible by using Docker containers. Start this service by running the following command in the terminal:
+
+```shell
+docker compose up --build
+```
+
+Confirm that LakeFS is running by opening a browser on the following website: http://localhost:8000.
+
+## Dataset
+
+The dataset used in this example is the NYC Taxi Trip dataset. Download the `.parquet` file from here: https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2022-02.parquet
+
+Once downloaded, go to the `demo` repository and upload the dataset to the following path: `taxi_data/input/`:
+![](asset/upload_taxi.png)
+
+## Running the Application
+
+All that remains is to `build` and `run` the application with:
+
+```shell
+cargo build
+cargo run
+```
+
+Once it has completed running, you will be able to see an output in the terminal window. You will also be able to find a set of files written to the following path: http://localhost:8000/repositories/demo/objects?ref=main&path=taxi_data%2Foutput%2F.
---
+++ src/main.rs
@@ -0,0 +1,75 @@
+use std::env;
+use std::sync::Arc;
+use datafusion::dataframe::DataFrameWriteOptions;
+
+use datafusion::datasource::file_format::parquet::ParquetFormat;
+use datafusion::datasource::listing::ListingOptions;
+use datafusion::error::Result;
+use datafusion::prelude::*;
+use datafusion_common::{FileType, GetExt};
+
+use object_store::aws::AmazonS3Builder;
+use url::Url;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // load environment variables
+    dotenv::dotenv().ok();
+
+    // create local execution context
+    let ctx = SessionContext::new();
+
+    // define region and repository to which your credentials have GET and PUT access
+    let region = "us-east-1";
+    let repo = "demo";
+    let branch = "main";
+
+    let s3a = AmazonS3Builder::new()
+        .with_bucket_name(repo)
+        .with_region(region)
+        .with_access_key_id(env::var("AWS_ACCESS_KEY_ID").unwrap())
+        .with_secret_access_key(env::var("AWS_SECRET_ACCESS_KEY").unwrap())
+        .with_endpoint("http://localhost:8000")
+        .with_allow_http(true) // enable http endpoints
+        .build()?;
+
+    let path = format!("s3a://{repo}");
+    let s3a_url = Url::parse(&path).unwrap();
+    let arc_s3a = Arc::new(s3a);
+    ctx.runtime_env()
+        .register_object_store(&s3a_url, arc_s3a.clone());
+
+    let path = format!("s3a://{repo}/{branch}/taxi_data/input/yellow_tripdata_2022-02.parquet");
+    let file_format = ParquetFormat::default().with_enable_pruning(true);
+    let listing_options = ListingOptions::new(Arc::new(file_format))
+        .with_file_extension(FileType::PARQUET.get_ext());
+    ctx.register_listing_table("taxi_table", &path, listing_options, None, None)
+        .await?;
+
+    let df = ctx.sql("SELECT * from taxi_table").await?;
+
+    // write as parquet to lakefs
+    let out_path = format!("s3a://{repo}/{branch}/taxi_data/output/parquet/");
+    df.clone().write_parquet(&out_path, DataFrameWriteOptions::new(), None).await?;
+
+    // write as JSON to lakefs
+    let json_out = format!("s3a://{repo}/{branch}/taxi_data/output/json");
+    df.clone().write_json(&json_out, DataFrameWriteOptions::new(), None).await?;
+
+    // write as csv to lakefs
+    let csv_out = format!("s3a://{repo}/{branch}/taxi_data/output/csv");
+    df.write_csv(&csv_out, DataFrameWriteOptions::new(), None).await?;
+
+    let file_format = ParquetFormat::default().with_enable_pruning(true);
+    let listing_options = ListingOptions::new(Arc::new(file_format))
+        .with_file_extension(FileType::PARQUET.get_ext());
+    ctx.register_listing_table("table_table2", &out_path, listing_options, None, None)
+        .await?;
+
+    let df = ctx
+        .sql("SELECT * FROM table_table2")
+        .await?;
+
+    df.show_limit(20).await?;
+    Ok(())
+}
